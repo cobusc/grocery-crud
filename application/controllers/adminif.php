d7b330ed9ba71d7e84cb830720b4b68c6753cdfe
Cobus Carstens <cobusc@miranetworks.net>
2012-04-29 23:41:22 +0200

    auto_table function in the admin interface will now set rudementary relations for foreign keys (PostrgreSQL only, for now)

--- application/controllers/adminif.php
+++ application/controllers/adminif.php
@@ -44,6 +44,22 @@ class AdminIf extends CI_Controller {
 
             $crud->set_table($table_name);
 
+            //$sql = "select b.table_name, b.column_name from information_schema.table_constraints a, information_schema.constraint_column_usage b where a.constraint_type = 'FOREIGN KEY' and a.table_name = '$table_name' and a.constraint_name=b.constraint_name;";
+            $sql = "SELECT tc.constraint_name,
+                           tc.table_name,
+                           kcu.column_name,
+                           ccu.table_name AS foreign_table_name,
+                           ccu.column_name AS foreign_column_name
+                      FROM information_schema.table_constraints AS tc 
+                      JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name
+                      JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name
+                     WHERE constraint_type = 'FOREIGN KEY'
+                       AND tc.table_name='$table_name'";
+            foreach($this->db->query($sql)->result() as $relation)
+            {
+                $crud->set_relation($relation->column_name,$relation->foreign_table_name,$relation->foreign_column_name); 
+                //break;
+            }
             $output = $crud->render();
 
             $this->_example_output($output);
